b4172b940dcbc58bada84eb02e1a2dccc014d6ae
Evel25 <bi3.h300@gmail.com>
2020-02-24 18:00:02 +0100

    adding the bootsrap and setup the envirement

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import App from './App.vue'
+import './plugins/bootstrap-vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 
 Vue.config.productionTip = false
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 new Vue({
   render: h => h(App),
